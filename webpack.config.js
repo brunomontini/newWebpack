2e13991d74d028f3c95298281cd370428cb058a7
Bruno Montini <brunomontini96@gmail.com>
2022-05-18 21:21:52 -0300

    include boilerplate webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+
+module.exports={
+  mode: "development",
+
+  entry:{
+    index: "./src/index.js"
+  },
+  output:{
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module:{
+    rules: [
+      {
+        test: /\$js/,
+        use: ["babel-loader"]
+      }
+    ]
+  },
+  devServer:{
+    static:{
+      directory: path.join(__dirname,"dist")
+    }
+  }
+}
